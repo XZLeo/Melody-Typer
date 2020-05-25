30ed96eb745089090ddcedc77a348e400104f876
XZLeo <13718722639@163.com>
2020-05-25 14:06:38 +0800

    enter

--- MelodyGenerator/AdbMelodyGenerator.py
+++ MelodyGenerator/AdbMelodyGenerator.py
@@ -7,32 +7,15 @@
 #3.1. 首先要手机连上adb，确定拨号界面每个键的
 
 # 感觉每个都转换成按键不是很高效的样子....
-
+import sys
 import time
 import os
-#开头将adb加入临时环境变量,用sys.path.append无效
-ADB_path = "d:/ADB/"
-os.environ["PATH"] = ADB_path
-# 测试有效
-#os.system("adb shell input tap 250 1200")
-
-current_path = 'd:/NewPython/MelodyGenerator/'
-current_file = {"1":"HappyBirthday.txt", "2":"LittleStar.txt"}
-
-#构建字典存储按键对应坐标
-X = {'0':530, '1':250, '2':530, '3':860, '4':250, '5':530, 
-    '6':860, '7':250, '8':530, '9':860, '*':250, '#':860}
-Y = {'0':1800, '1':1200, '2':1200, '3':1200, '4':1400, '5':1400,
-    '6':1400, '7':1600, '8':1600, '9':1600, '*':1800, '#':1800}
 
 def filereader(path):
     #读取乐谱，返回list
     f = open(path)
     score = f.read() 
-    print('type:', type(score)) #读进来是个字符串
-    print(score)
-    return score
-
+    return score 
 
 def decoder(score):
     #读取list，翻译成具有等待时间的、节奏的adb命令
@@ -47,12 +30,39 @@ def decoder(score):
             os.system("adb shell input tap {x} {y}".format(x=X[note], y=Y[note]))
         else:
             pass
+    os.system("adb shell input swipe 1000 2000 1001 2001 1000") #删除拨号
     return
 
-path = current_path + current_file
-score = filereader(path)
-decoder(score)
 
-# 加入多遍循环，直到键盘按键
-# 搜集更多的手机谱
-# 弄个输入可以选择对应的谱子，首先需要一个字典将已有的名字放进去
+if __name__ == '__main__':
+    #开头将adb加入临时环境变量,用sys.path.append无效
+    ADB_path = "d:/ADB/"
+    os.environ["PATH"] = ADB_path
+    # 测试有效
+    #os.system("adb shell input tap 250 1200")
+
+    current_path = 'd:/NewPython/MelodyGenerator/'
+    current_file = {"1":"HappyBirthday.txt", "2":"LittleStar.txt", "3":"Jasmine.txt", 
+                    "4":"CaribbeanPirate.txt", "5":"taiguan.txt"}
+
+    #构建字典存储按键对应坐标
+    X = {'0':530, '1':250, '2':530, '3':860, '4':250, '5':530, 
+        '6':860, '7':250, '8':530, '9':860, '*':250, '#':860}
+    Y = {'0':1800, '1':1200, '2':1200, '3':1200, '4':1400, '5':1400,
+        '6':1400, '7':1600, '8':1600, '9':1600, '*':1800, '#':1800}
+    
+    while True:
+        flag = os.popen("adb devices")
+        a = flag.read()
+        print(a)
+        if len(a) > 27:
+            notification = "1:HappyBirthday, 2:LittleStar, 3:Jasmine,\n4:CaribbeanPirate 5:黑人抬棺:\n"
+            music_name = input(notification)
+            path = current_path + current_file[music_name]
+            score = filereader(path)
+            decoder(score)
+        else:
+            print("Chec your mobikephone connection")
+            break
+
+    # 如何提高按键频率
---
+++ MelodyGenerator/argv.py
@@ -0,0 +1,66 @@
+import os
+
+ADB_path = "d:/ADB/"
+os.environ["PATH"] = ADB_path
+
+
+# 用python3获取系统命令的输出
+import time
+import os
+begin = time.time()
+os.system('adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 adb shell input tap 250 1200 && adb shell input tap 250 1200 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400 && adb shell input tap 250 1400')
+print(time.time() - begin)
+
+begin = time.time()
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+os.system('adb shell input tap 250 1200')
+os.system("adb shell input tap 250 1200")
+os.system('adb shell input tap 250 1400')
+print(time.time() - begin)
+
+# 两种方式的速度差不多，前者稍快一点;越长，时间差越大，大概按键48次，前者快了2s
+
