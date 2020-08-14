0c3275bed7e819c136aa4d38103397aaf9ad578f
Leo Xiong <39155356+XZLeo@users.noreply.github.com>
2020-08-14 14:24:31 +0800

    Delete somescript.py

--- somescript.py
+++
@@ -1,9 +0,0 @@
-import sys
-text = sys.stdin.read()
-words = text.split()
-wordcount = len(words)
-print('wordcount:', wordcount)
-# powershell里是兼容Unix shell的命令的
-#  cat ss.txt | python somescript.py | sort 
-# 统计ss.txt里的单词书
-
